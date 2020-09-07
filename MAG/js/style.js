97187d6b75585aba20ed314e3d04fdba096545ad
rajrobust <raj1999gupta@gmail.com>
2020-09-07 23:56:46 +0530

    About

--- MAG/js/style.js
+++ MAG/js/style.js
@@ -3,6 +3,37 @@
   
 NAVIGATION
 // ================================================*/
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            
+            $("nav").removeClass("mag-top-nav");
+            
+
+        } else {
+            // show nav
+            $("nav").addClass("mag-top-nav");
+            
+        }
+    });
+});
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+    //    return id pe pohchna
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
 $(function(){
     
     $(".navbar-collapse ul li a").on("click touch", function(){
