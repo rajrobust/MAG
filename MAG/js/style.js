3e5207ff2d80df5675439881647846b5e47154ce
rajrobust <raj1999gupta@gmail.com>
2020-09-07 16:10:57 +0530

    Initial commit header

---
+++ MAG/js/style.js
@@ -0,0 +1,33 @@
+
+/*
+  
+NAVIGATION
+// ================================================*/
+$(function(){
+    
+    $(".navbar-collapse ul li a").on("click touch", function(){
+       
+        $(".navbar-toggle").click();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
